7741a11b7e952807e94b7a409d2935ae533ee6fa
oriyomi.solaja@gmail.com <oriyomi.solaja@gmail.com>
2026-03-18 19:50:26 +0000

    Created function to get video ids

--- video_stats.py
+++ video_stats.py
@@ -7,6 +7,7 @@ load_dotenv(dotenv_path=".env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlistID():
     
@@ -31,6 +32,49 @@ def get_playlistID():
     except requests.exceptions.RequestException as e:
         raise e
    
-   
+
+
+def get_video_ids(playlistID):
+    
+    #initialise container for all videos id as an emtpty list
+    video_ids = []
+    
+    #create page token variable and initialise to None 
+    pagetoken = None
+    
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistID}&key={API_KEY}"  
+    
+    try:
+        #start a loop that will break when there are no more page token to go through(the first iteration will be false)
+        while True:
+            
+            url = base_url
+            
+            if pagetoken:
+                #if a pagetoken is returned the url will append the page token part to the main url
+                url += f"&pageToken={pagetoken}"
+                
+            response = requests.get(url)
+    
+            response.raise_for_status()
+                
+            data = response.json()
+                
+                #get the value from the item scheme, use for loop to loop through the item scheme
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]['videoId']
+                video_ids.append(video_id)
+                    
+            pagetoken = data.get('nextPageToken')
+                
+            if not pagetoken:
+                break
+            
+        return video_ids
+        
+    except requests.exceptions.RequestException as e:
+        raise e
+    
 if __name__=="__main__":
-    get_playlistID()
+    playlistID = get_playlistID()
+    get_video_ids(playlistID)
