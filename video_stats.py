4222d7ed7dd72fd43d70016d842b7765e6a0847f
oriyomi.solaja@gmail.com <oriyomi.solaja@gmail.com>
2026-03-18 20:41:23 +0000

    created mian function to extract data

--- video_stats.py
+++ video_stats.py
@@ -25,7 +25,7 @@ def get_playlistID():
         channel_items = data["items"][0]
         
         channel_playlistID = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
-        print(channel_playlistID)
+        #print(channel_playlistID)
 
         return channel_playlistID
     
@@ -75,6 +75,56 @@ def get_video_ids(playlistID):
     except requests.exceptions.RequestException as e:
         raise e
     
+
+
+#define main function for extracting data
+
+def extract_video_data(video_ids):
+    
+    extracted_data = []
+    
+    #create a helper function to split the video id list into batches(the paramenter for video ids can only take up tp 50 video id)
+    def batch_list(video_id_lst, batch_size):
+        #define a for loop that will loop through the list and batches of size batch size
+        for video_id in range(0,len(video_id_lst), batch_size):
+            yield video_id_lst[video_id : video_id + batch_size]
+    
+    try:
+     #loop through the whole list in batches, at each batch defining the concatenated video ID string using the Python Join method, which we will then use to build the url
+        for batch in batch_list(video_ids, maxResults):
+            #join video ids into a comma seperated string as per the docs
+            video_ids_str = ",".join(batch)
+            
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+            
+            response = requests.get(url)
+    
+            response.raise_for_status()
+                
+            data = response.json()
+            
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails =item['contentDetails']
+                statistics = item['statistics']
+             
+                #define dictionary that will take the variables from the loop above   
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None),
+                    "likeCount": statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None)     
+                }
+                
+                extracted_data.append(video_data)
+        return extracted_data   
+    except requests.exceptions.RequestException as e:
+        raise e
 if __name__=="__main__":
     playlistID = get_playlistID()
-    get_video_ids(playlistID)
+    video_ids = get_video_ids(playlistID)
+    extract_video_data(video_ids)
