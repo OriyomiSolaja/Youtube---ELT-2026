d4f25ce987f767f53ec84fbe61aa0f6900fbf9a1
oriyomi.solaja@gmail.com <oriyomi.solaja@gmail.com>
2026-03-27 11:24:42 +0000

    Created helper fucntion for datawarehouse. completed extract functions

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,14 +2,21 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
-load_dotenv(dotenv_path=".env")
+#import os
+#from dotenv import load_dotenv
+#load_dotenv(dotenv_path=".env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+from airflow.decorators import task
+
+from airflow.models import Variable
+
+#API_KEY = os.getenv("API_KEY")
+API_KEY = Variable.get("API_KEY")
+#CHANNEL_HANDLE = "MrBeast"
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlistID():
     
     try:
@@ -34,7 +41,7 @@ def get_playlistID():
         raise e
    
 
-
+@task
 def get_video_ids(playlistID):
     
     #initialise container for all videos id as an emtpty list
@@ -77,9 +84,8 @@ def get_video_ids(playlistID):
         raise e
     
 
-
 #define main function for extracting data
-
+@task
 def extract_video_data(video_ids):
     
     extracted_data = []
@@ -127,7 +133,8 @@ def extract_video_data(video_ids):
     
     except requests.exceptions.RequestException as e:
         raise e
-    
+ 
+@task   
 def save_to_json(extracted_data):
     #save json to file path 'data'. we also specify in json file name the day the file was generated
     file_path = f"./data/YT_data_{date.today()}.json"
