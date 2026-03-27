c62ec10628a637e4819ab8767d7d8c3f229c2b21
oriyomi.solaja@gmail.com <oriyomi.solaja@gmail.com>
2026-03-27 16:53:34 +0000

    created helper functions for creating tables & schemas

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -1,6 +1,8 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from pyscopg2.extras import RealDictCursor
 
+table = "yt_api"
+
 
 def get_conn_cursor():
     hook = PostgresHook(postgres_conn_id="postgres_db_yt_elt", database="elt_db")
@@ -11,3 +13,63 @@ def get_conn_cursor():
 def close_conn_cursor(conn,cur):
     cur.close()
     conn.close()
+    
+def create_schema(schema):
+    
+    conn, cur = get_conn_cursor()
+    
+    schema_sql = f"CREATE SCHEMA IF NOT EXISTS {schema}"
+    
+    cur.execute(schema_sql)
+    
+    conn.commit()
+    
+    close_conn_cursor(conn, cur)
+    
+def create_table(schema):
+    
+    conn, cur = get_conn_cursor()
+    
+    if schema == 'staging':
+        table_sql = f"""
+                CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                    "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                    "Video_Title" TEXT NOT NULL,
+                    "Upload_Date" TIMESTAMP NOT NULL,
+                    "Duration" VARCHAR(20) NOT NULL,
+                    "Video_Views" INT,
+                    "Likes_count" INT,
+                    "Comments_count INT
+                );
+            """
+    else:
+        table_sql = f"""
+                CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                    "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                    "Video_Title" TEXT NOT NULL,
+                    "Upload_Date" TIMESTAMP NOT NULL,
+                    "Duration" TIME NOT NULL,
+                    "Video_Type" VARCHAR(10) NOT NULL,
+                    "Video_Views" INT,
+                    "likes_count" INT,
+                    "Comments_count INT
+                );
+            """
+    cur.execute(table_sql)
+    
+    conn.commit()
+    
+    close_conn_cursor(conn, cur)
+    
+    
+#define function to get video ids
+def get_video_ids(cur, schema):
+    
+    cur.execute(f"""SELECT "Video_ID FROM {schema}.{table};""")
+    ids = cur.fetchall()
+    
+    #write a list comprehension to get a new list from the video id dictionary that will be returned
+    
+    video_ids =[row['Video_ID'] for row in ids]
+    
+    return video_ids
