e238212382635ed5dde0b85b8695d7b59e4920ae
oriyomi.solaja@gmail.com <oriyomi.solaja@gmail.com>
2026-03-29 21:10:19 +0100

    testing & data quality included

--- dags/main.py
+++ dags/main.py
@@ -1,8 +1,15 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
-from api.video_stats import get_playlistID, get_video_ids, extract_video_data, save_to_json
 
+from api.video_stats import (
+    get_playlistID,
+    get_video_ids, 
+    extract_video_data, 
+    save_to_json
+    )
+
+from dataquality.soda import yt_elt_data_quality
 from datawarehouse.dwh import staging_table, core_table
 # Define the local timezone
 local_tz = pendulum.timezone("Europe/London")
@@ -21,6 +28,10 @@ default_args = {
     "start_date": datetime(2026, 3, 22, tzinfo=local_tz),
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
+
+# Variables
+staging_schema = "staging"
+core_schema = "core"
 
 # DAG 1: produce_json
 with DAG(
@@ -55,3 +66,19 @@ with DAG(
 
     # Define dependencies
     update_staging >> update_core 
+    
+# DAG 3: data_quality
+with DAG(
+    dag_id="data_quality",
+    default_args=default_args,
+    description="DAG to check the data quality on both layers in the database",
+    catchup=False,
+    schedule=None,
+) as dag_quality:
+
+    # Define tasks
+    soda_validate_staging = yt_elt_data_quality(staging_schema)
+    soda_validate_core = yt_elt_data_quality(core_schema)
+
+    # Define dependencies
+    soda_validate_staging >> soda_validate_core
